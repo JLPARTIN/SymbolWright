437c78ba5733a5f59663b6783ba42762a4283ccb
JLPARTIN <119410089+JLPARTIN@users.noreply.github.com>
2026-05-28 06:39:55 -0500

    feat: add public TypeScript entrypoint

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export {
+  CODEMIND_AJNA_CAPABILITY_NAME,
+  CODEMIND_PLATFORM_NAME,
+  getCodemindFoundationSnapshot,
+} from './codemind-foundation.js';
+export type {
+  CodemindFoundationSnapshot,
+  CodemindRuntimePosture,
+} from './codemind-foundation.js';
